8e4c9bbcf29083bb56d7c8bf21e254f5ba95f687
dev1pavlo <pavlo.vasylenko.dev@gmail.com>
2023-11-09 17:03:11 +0200

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return process.env.ENV_TEST;
+  }
+}
